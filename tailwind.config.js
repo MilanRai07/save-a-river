2373283b65291bdfa1bcaa22412e728bb3ebe0cf
milanrai <milanraiyt07@gmail.com>
2024-08-07 10:21:16 +0545

    image issue fixed and website preloader

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'scene': "url('./images/riverScene.jpg')"
+        'scene': "url('/images/riverScene.jpg')"
       },
     },
   },
